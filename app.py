ed0da9c925a908a3bd37bbe9f8ec854f9fc9a1da
gourav-chalotra <chalgourav82@gmail.com>
2025-06-19 10:55:16 +0530

    second

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 from flask import Flask, request, jsonify
-for sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
 import joblib
 import os
 import logging
-
